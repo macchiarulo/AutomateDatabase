732227f5a0c8e345fbbe58366baee04e27858fce
macchiarulo <40037374+macchiarulo@users.noreply.github.com>
2018-11-24 17:38:36 -0500

    Create auto.py

---
+++ auto.py
@@ -0,0 +1,19 @@
+# AutomateDatabase
+Automate Database to AWS S3 Bucket (Python, MySQL, PHP)
+import pymysql
+
+conn = pymysql.connect(host= '[host]',user='[username]',password='[password]',db='[database_name]')
+a = conn.cursor()
+
+sql = 'SELECT * from [table_name]'
+a.execute(sql)
+
+data = a.fetchone()
+print(data)
+
+data_folder = "[php_local_host_path]"
+file_to_open = data_folder + "[text_file_name_saved_in_php_localhost_to_refresh]"
+f = open(file_to_open, 'w')
+f.write(str(data))
+f.close()
+
